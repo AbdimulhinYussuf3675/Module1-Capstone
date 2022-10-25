96aea6f123f349091deb74a7075d8d00d0918a4a
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2022-10-25 12:32:32 +0300

    Add humberger menu functionality

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const hamburgerBtn = document.querySelector('.icon-menu-mobile img');
+const crossBtn = document.querySelector('.cross img');
+const showMoreBtn = document.querySelector('.testimonial-btn button');
+const mobileMenu = document.querySelector('.nav-mobile');
+const mobileMenuElements = document.querySelectorAll('.nav-mobile .container li');
+const testimonialContainer = document.querySelector('.testimonial-container');
+hamburgerBtn.addEventListener('click', () => {
+  mobileMenu.style.display = 'flex';
+  hamburgerBtn.style.display = 'none';
+});
+crossBtn.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+  hamburgerBtn.style.display = 'block';
+});
+
+mobileMenuElements.forEach((menuElement) => {
+  menuElement.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+    hamburgerBtn.style.display = 'block';
+  });
+});

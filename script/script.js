b392d317aa7b1b351c8857108edbbf4adabb618e
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2022-10-27 13:25:28 +0300

    Update for deployment

--- script/script.js
+++ script/script.js
@@ -2,42 +2,42 @@ const claimObjs = [{
   name: 'Hassan Abdi',
   comment: 'If you have some events and want to hire its coverage just try Adam Studio, you will have experience on your own',
   work: 'Graphic Designer',  
-  imgsrc: '/Images/Nav/speaker_06.png',
+  imgsrc: 'Images/Nav/4.png',
   imgdescription: 'person1',
 },
 {
   name: 'Binamin Solo',
   comment: 'I had my wedding covered and got the videos and images on time compared to other studios.',
   work: 'Software Engeneer',
-  imgsrc: '/Images/Nav/speaker_05.png',
+  imgsrc: 'Images/Nav/s.png',
   imgdescription: 'person2',
 },
 {
   name: 'Rahma S.',
   comment: 'Your workers give their best when hired, thumbs up.I recommend anyone here',
   work: 'Nurse',
-  imgsrc: '/Images/Nav/speaker_04.png',
+  imgsrc: 'Images/Nav/6.png',
   imgdescription: 'person3',
 },
 {
   name: 'Osman G.',
   comment: 'I received the package today. Thank you. I tried the pants on with my new braces. The pants are beautifully done and fit perfectly. Congratulations on the excellent tailoring your team accomplished',
   work: 'Musician',
-  imgsrc: '/Images/Nav/speaker_03.png',
+  imgsrc: 'Images/Nav/7.png',
   imgdescription: 'person4',
 },
 {
   name: 'Hussein D.',
   comment: 'You dont have to stress on your events coverage just contact Adam Studio, Stop over researching. I recommend you to Adam Studio!',
   work: 'Actor',
-  imgsrc: '/Images/Nav/speaker_02.png',
+  imgsrc: 'Images/Nav/45.png',
   imgdescription: 'person5',
 },
 {
   name: 'Adan B.',
   comment: 'If their is a photographers and videographers i can think to hire on my evens, i would never think twice about Adam studio Clients are served with respect. I recommend anyone here',
   work: 'Electrical Engeener',
-  imgsrc: '/Images/Nav/speaker_01.png',
+  imgsrc: 'Images/Nav/9.png',
   imgdescription: 'person6',
 }];
 const hamburgerBtn = document.querySelector('.icon-menu-mobile img');
@@ -92,9 +92,9 @@ if (showMoreBtn !== null) {
       }
     });
     if (claims[2].style.display === 'none') {
-      showMoreBtn.innerHTML = 'MORE <img src="/Images/Nav/less.png" alt="down icon">';
+      showMoreBtn.innerHTML = 'MORE <img src="Images/Nav/less.png" alt="down icon">';
     } else {
-      showMoreBtn.innerHTML = 'LESS <img src="/Module1-Capstone/Images/Nav/more.jpeg" alt="down icon">';
+      showMoreBtn.innerHTML = 'LESS <img src="Images/Nav/more.jpeg"  alt="up icon">>';
     }
   });
 }
